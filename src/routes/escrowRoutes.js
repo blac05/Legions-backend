381e759b80d3dd8c65de0cbd4794ff627e221eee
Joe Jae <blaqj23@gmail.com>
2026-07-06 03:16:06 +0000

    Update escrowRoutes.js

--- src/routes/escrowRoutes.js
+++ src/routes/escrowRoutes.js
@@ -3,6 +3,7 @@ import { requireAuth, requireVerifiedEmail } from "../middleware/auth.js";
 import {
   createEscrow, listMyEscrows, getEscrow, agreeToEscrow,
   toggleCondition, releaseMilestone, flagBreach,
+  requestCancellation, withdrawCancellation,
 } from "../controllers/escrowController.js";
 
 const router = express.Router();
@@ -15,5 +16,7 @@ router.post("/:id/agree", requireVerifiedEmail, agreeToEscrow);
 router.patch("/:id/milestones/:milestoneId/conditions/:conditionId", toggleCondition);
 router.post("/:id/milestones/:milestoneId/release", releaseMilestone);
 router.post("/:id/dispute", flagBreach);
+router.post("/:id/cancel", requestCancellation);
+router.post("/:id/cancel/withdraw", withdrawCancellation);
 
 export default router;
