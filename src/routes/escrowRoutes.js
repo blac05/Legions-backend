299ccf8d2d57cf818d5ecce639681a7aaf96f12b
Joe Jae <blaqj23@gmail.com>
2026-07-10 01:20:13 +0000

    Update escrowRoutes.js

--- src/routes/escrowRoutes.js
+++ src/routes/escrowRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { requireAuth, requireVerifiedEmail } from "../middleware/auth.js";
+import { financialActionLimiter } from "../middleware/rateLimiters.js";
+import { idempotent } from "../middleware/idempotency.js";
 import {
   createEscrow, listMyEscrows, getEscrow, agreeToEscrow,
   toggleCondition, releaseMilestone, flagBreach,
@@ -14,9 +16,9 @@ router.get("/", listMyEscrows);
 router.get("/:id", getEscrow);
 router.post("/:id/agree", requireVerifiedEmail, agreeToEscrow);
 router.patch("/:id/milestones/:milestoneId/conditions/:conditionId", toggleCondition);
-router.post("/:id/milestones/:milestoneId/release", releaseMilestone);
+router.post("/:id/milestones/:milestoneId/release", financialActionLimiter, idempotent("release_milestone"), releaseMilestone);
 router.post("/:id/dispute", flagBreach);
-router.post("/:id/cancel", requestCancellation);
+router.post("/:id/cancel", financialActionLimiter, requestCancellation);
 router.post("/:id/cancel/withdraw", withdrawCancellation);
 
 export default router;
